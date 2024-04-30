d050fdd8210c75956a720dd3efdbdeb8b7690a7b
olucasoeiro <101152798+olucasoeiro@users.noreply.github.com>
2024-04-30 11:33:44 -0300

    new images

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function toggleMode() {
     const img = document.querySelector('#profile img')
   
     if (html.classList.contains('light')) {
-      img.setAttribute('src', './assets/avatar-light.png')
+      img.setAttribute('src', './assets/profile-light.jpg')
     } else {
-      img.setAttribute("src", "./assets/avatar.png")
+      img.setAttribute("src", "./assets/profile-dark.jpg")
     }
   }
